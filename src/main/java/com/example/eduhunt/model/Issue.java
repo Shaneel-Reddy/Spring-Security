0a048308f45ea1785564659c9a20845796952807
Shaneel Reddy <shaneelreddy320@gmail.com>
2024-12-17 18:47:29 +0530

    initial commit

---
+++ src/main/java/com/example/eduhunt/model/Issue.java
@@ -0,0 +1,16 @@
+package com.example.eduhunt.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Issue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "assignee_id")
+    private User assignee;
+}
